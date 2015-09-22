6dd27de5d655a7b463da660f1945ddcb5485632c
cfisher <cfisher@moredirect.com>
2015-09-22 07:37:27 -0400

    Update breakpoints. Minify css
    Change-Id: I257ffa5b08022132dff1c9b3b3e5c89c2dc5c74e

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,30 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var bower = require('gulp-bower');
+var watch = require('gulp-watch');
+var cssMin = require('gulp-minify-css');
 
-// Gulp Sass Task 
-gulp.task('sass', ['bower'], function() {
-  return gulp.src('./scss/{,*/}*.{scss,sass}')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-      errLogToConsole: true
-    }))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css'));
-})
-
- 
+// Bower install 
 gulp.task('bower', function() {
   return bower()
     .pipe(gulp.dest('./bower_components/'))
 });
 
+// Gulp Sass Task 
+gulp.task('sass', ['bower'], function() {
+  return gulp.src('./scss/{,*/}*.{scss,sass}')
+//    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+//    .pipe(sourcemaps.write())
+    .pipe(cssMin({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./css'));
+});
+
+
 gulp.task('init', ['bower', 'sass'])
 gulp.task('default', ['sass']);
+gulp.task('watch', function () {
+  return gulp.watch('./scss/**/*.scss', ['sass']);
+});
