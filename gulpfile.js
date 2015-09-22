1120e3fc2315273329fa7d14573ec137f5c9ded3
Cory Fisher <coryjamesfisher@gmail.com>
2015-09-21 21:11:42 -0400

    Add bower task to gulpfile. Also added init task to do setup

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp');
 // Include Our Plugins
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var bower = require('gulp-bower');
 
 // Gulp Sass Task 
 gulp.task('sass', function() {
@@ -15,10 +16,11 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./css'));
 })
 
-/*gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css'));
-});*/
+ 
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest('bower_components/'))
+});
 
+gulp.task('init', ['sass', 'bower'])
 gulp.task('default', ['sass']);
