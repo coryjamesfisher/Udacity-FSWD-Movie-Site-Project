3c7741502689da66c33f812c96d15102e26a8139
cfisher <cfisher@moredirect.com>
2015-09-23 06:41:07 -0400

    Use normalize.css and update gulpfile
    Change-Id: Ib6270246430e2f1d50feca9f26d69a8395cecfd4

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
 var bower = require('gulp-bower');
 var watch = require('gulp-watch');
 var cssMin = require('gulp-minify-css');
+var concat = require('gulp-concat');
+var merge = require('merge-stream');
 
 // Bower install 
 gulp.task('bower', function() {
@@ -15,14 +16,19 @@ gulp.task('bower', function() {
 
 // Gulp Sass Task 
 gulp.task('sass', ['bower'], function() {
-  return gulp.src('./scss/{,*/}*.{scss,sass}')
-//    .pipe(sourcemaps.init())
+  var sassStream,
+      cssStream;
+
+  cssStream = gulp.src('./bower_components/normalize-css/normalize.css');
+  sassStream = gulp.src('./scss/{,*/}*.{scss,sass}')
     .pipe(sass({
       errLogToConsole: true
-    }))
-//    .pipe(sourcemaps.write())
-    .pipe(cssMin({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./css'));
+    }));
+
+  return merge(cssStream, sassStream)
+	.pipe(concat('styles.css'))
+    	.pipe(cssMin({compatibility: 'ie8'}))
+	.pipe(gulp.dest('./css'));
 });
 
 
