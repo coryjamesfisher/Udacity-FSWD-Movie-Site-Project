ccbdc63b5e3367dc3fdb79bd1551a5e1d3cce537
cfisher <cfisher@moredirect.com>
2015-09-23 08:14:13 -0400

    Include modernizr in build
    Change-Id: Ic5a40e9d9ddcad8f1ca932ae4a58a4aebf37ccb5

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var watch = require('gulp-watch');
 var cssMin = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var merge = require('merge-stream');
+var modernizr = require('gulp-modernizr');
 
 // Bower install 
 gulp.task('bower', function() {
@@ -31,8 +32,72 @@ gulp.task('sass', ['bower'], function() {
 	.pipe(gulp.dest('./css'));
 });
 
+gulp.task('js', function() {
+  return gulp.src('./node_modules/modernizr/src/*.js')
+	.pipe(modernizr({
+          "minify": true,
+          "options": [
+            "domPrefixes",
+            "prefixes",
+            "addTest",
+            "hasEvent",
+            "mq",
+            "prefixed",
+            "testAllProps",
+            "testProp",
+            "testStyles",
+            "html5shiv",
+            "setClasses"
+          ],
+          "feature-detects": [
+            "test/applicationcache",
+            "test/audio",
+            "test/canvas",
+            "test/canvastext",
+            "test/geolocation",
+            "test/hashchange",
+            "test/history",
+            "test/indexeddb",
+            "test/input",
+            "test/inputtypes",
+            "test/postmessage",
+            "test/svg",
+            "test/video",
+            "test/webgl",
+            "test/websockets",
+            "test/css/animations",
+            "test/css/backgroundsize",
+            "test/css/borderimage",
+            "test/css/borderradius",
+            "test/css/boxshadow",
+            "test/css/columns",
+            "test/css/flexbox",
+            "test/css/fontface",
+            "test/css/generatedcontent",
+            "test/css/gradients",
+            "test/css/hsla",
+            "test/css/multiplebgs",
+            "test/css/opacity",
+            "test/css/reflections",
+            "test/css/rgba",
+            "test/css/textshadow",
+            "test/css/transforms",
+            "test/css/transforms3d",
+            "test/css/transitions",
+            "test/storage/localstorage",
+            "test/storage/sessionstorage",
+            "test/storage/websqldatabase",
+            "test/svg/clippaths",
+            "test/svg/inline",
+            "test/svg/smil",
+            "test/workers/webworkers"
+          ]
+	}))
+	.pipe(gulp.dest('./js/'));
+});
 
-gulp.task('init', ['bower', 'sass'])
+
+gulp.task('init', ['bower', 'sass', 'js'])
 gulp.task('default', ['sass']);
 gulp.task('watch', function () {
   return gulp.watch('./scss/**/*.scss', ['sass']);
