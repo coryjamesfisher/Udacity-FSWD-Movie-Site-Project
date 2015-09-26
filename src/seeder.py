d60e038ac9d06f7740efa3b524006817c4ed5ddf
Cory Fisher <coryjamesfisher@gmail.com>
2015-09-26 14:30:24 -0400

    Allow seeder to load titles from already loaded movies

--- src/seeder.py
+++ src/seeder.py
@@ -2,19 +2,37 @@ import imdb
 import json
 import media
 import pickle
+import os.path
 
 # Get the users favorite movies
 def get_favorite_movie_titles():
     movie_titles = []
     done_adding = False
+
+    if os.path.isfile('cache/movie_seed.pickled'):
+	   print "Movies already in the database."
+           print "Press 'n' to wipe it and start over."
+           print "or press 'Enter' to add to it." 
+           if raw_input() != "n":
+               # Load the serialized movies
+               with open('cache/movie_seed.pickled','rb') as f:
+                   movies = pickle.load(f)
+                   for movie in movies:
+                       movie_titles.append(movie.title)
+
     while (done_adding != True):
+
         print "Type a favorite movie name (Press enter twice when done):",
         user_input = raw_input()
-    
+
         if len(user_input) == 0:
           done_adding = True
+        elif user_input.find(',,'):
+            for title in user_input.split(',,'):
+              movie_titles.append(title)
         else:
-          movie_titles.append(user_input)
+            movie_titles.append(user_input)
+
     return movie_titles
 
 # Look up movies on imdb
@@ -31,12 +49,8 @@ def imdb_lookup_movies(movie_titles):
 	result = results[0]
 	ia.update(result)
 
-        from pprint import pprint
-#        pprint (result['aka'])
-#        our_title = result['title']
-#	for alias_title in result['aka']:
-#            if alias_title.find('USA') != -1:
-#                   our_title = alias_title[:alias_title.find('::')] 
+#        from pprint import pprint
+#        pprint (vars(result))
 	our_title = result['aka'][0][:result['aka'][0].find('::')]
         
 	movie = media.Movie(
@@ -51,6 +65,8 @@ def imdb_lookup_movies(movie_titles):
 
 # Ask the user for their favorite movies
 favorite_movie_titles = get_favorite_movie_titles()
+
+print favorite_movie_titles
 
 # Look up the movies on IMDB
 movies = imdb_lookup_movies(favorite_movie_titles)
