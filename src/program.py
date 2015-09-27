94a54c16286f3881f5639a174430c5d10539beaa
Cory Fisher <coryjamesfisher@gmail.com>
2015-09-27 11:35:39 -0400

    Document program.py and fix style for pylint

--- src/program.py
+++ src/program.py
@@ -1,12 +1,19 @@
 # coding=UTF-8
+"""This file loads the movies from a pickle file and generates the web page
+"""
+
 import movie_site_generator
-import media
-import json
 import pickle
 
-# Load the serialized movies
-with open('cache/movie_seed.pickled','rb') as f:
-    movies = pickle.load(f)
+def load_movies():
+    """This method will load the movies from a pickle file
+    """
+
+    # Load the serialized movies
+    with open('cache/movie_seed.pickled', 'rb') as movie_file:
+
+        # Deserialize the movies
+        return pickle.load(movie_file)
 
 # Load the site in the browser
-movie_site_generator.open_movies_page(movies)
+movie_site_generator.open_movies_page(load_movies())
